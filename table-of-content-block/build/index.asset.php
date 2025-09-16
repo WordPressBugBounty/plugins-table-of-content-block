ef39c469e31073063bef3fc14d63c707b0ee6756
Sean Murphy <sean@iamseanmurphy.com>
2025-09-16 11:17:29 +0000

    1.0.6

---
+++ table-of-content-block/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-i18n'), 'version' => '9d3f8b51d753cb716c0d');
---
+++ table-of-content-block/build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => 'fb6f9b432a95346c90ed');
